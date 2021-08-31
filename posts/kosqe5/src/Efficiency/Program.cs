d3f71caef81362c2c2a77e16cbae445277aebc13
Andrey Akinshin <andrey.akinshin@gmail.com>
2021-08-31 12:07:04 +0000

    Deploying to gh-pages from  @ bf61c51bfe80b6d79c98ebbc8e425e8798923d24 🚀

---
+++ posts/kosqe5/src/Efficiency/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks.Sources;
+using Perfolizer.Common;
+using Perfolizer.Mathematics.Common;
+using Perfolizer.Mathematics.Distributions.ContinuousDistributions;
+using Perfolizer.Mathematics.QuantileEstimators;
+using Perfolizer.Mathematics.Reference;
+using Perfolizer.Mathematics.Sequences;
+
+namespace Efficiency
+{
+    class Program
+    {
+        static void Main()
+        {
+            // var d = new BetaDistribution(2, 10);
+            // var s = new Sample(d.Random().Next(9));
+            // var hd = new HarrellDavisQuantileEstimator();
+            // var thd4 = new KosQuantileEstimator(4, KosQuantileEstimator.Type.Thd);
+            // var b6 = new KosQuantileEstimator(6, KosQuantileEstimator.Type.Beta);
+            // var thd6 = new KosQuantileEstimator(6, KosQuantileEstimator.Type.Thd);
+            // var thd16 = new KosQuantileEstimator(16, KosQuantileEstimator.Type.Thd);
+            // var p = 0.5;
+            // Console.WriteLine("True  : " + d.Quantile(p));
+            // Console.WriteLine("THD4  : " + thd4.GetQuantile(s, p));
+            // Console.WriteLine("HD    : " + hd.GetQuantile(s, p));
+            // Console.WriteLine("B6    : " + b6.GetQuantile(s, p));
+            // Console.WriteLine("THD6  : " + thd6.GetQuantile(s, p));
+            // Console.WriteLine("THD16 : " + thd16.GetQuantile(s, p));
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            InitCulture();
+            Simulate();
+        }
+
+        
+
+        private void Simulate()
+        {
+            var estimators = Settings.Estimators;
+            var probabilities = GenerateSequence(1, 99, Settings.ProbabilitiesStep)
+                .Select(p => (Probability)(p / 100.0)).ToArray();
+            int[] sampleSizes = GenerateSequence(Settings.MinSampleSize, Settings.MaxSampleSize, Settings.SampleSizeStep).ToArray();
+
+            var sw = Stopwatch.StartNew();
+
+            Run("LightAndHeavy", estimators, probabilities, sampleSizes,
+                LightAndHeavyDistributionSet.Instance.Distributions);
+            sw.Stop();
+            Logger.WriteLine($"Total Elapsed: {sw.Elapsed}");
+        }
+
+        private void Run(
+            string name,
+            IReadOnlyList<IQuantileEstimator> estimators,
+            Probability[] probabilities,
+            int[] sampleSizes,
+            ReferenceDistribution[] referenceDistributions)
+        {
+            SavePdf(name, referenceDistributions);
+            SaveDescription(name, referenceDistributions);
+            RunAndSaveEfficiency(name, estimators, probabilities, sampleSizes, referenceDistributions);
+        }
+
+        private void RunAndSaveEfficiency(string name, IReadOnlyList<IQuantileEstimator> estimators,
+            Probability[] probabilities,
+            int[] sampleSizes, ReferenceDistribution[] referenceDistributions)
+        {
+            string fileName = $"{name}_Efficiency.csv";
+            Logger.WriteLine($"Start simulation for {name}");
+
+            var simulator = new QuantileEstimatorEfficiencySimulator();
+            var sw = Stopwatch.StartNew();
+            var result = simulator.Run(estimators, probabilities, sampleSizes, referenceDistributions, progress =>
+            {
+                double elapsedSec = sw.Elapsed.TotalSeconds;
+                double etaSec = elapsedSec * (100 - progress) / progress;
+                Logger.WriteLine($"Progress: {progress:N2}% Elapsed:{elapsedSec:N1}s ETA:{etaSec:N1}s");
+            });
+            sw.Stop();
+            Logger.WriteLine($"Elapsed: {sw.Elapsed.TotalSeconds:N1}s");
+
+            result.SaveToCsv(fileName);
+            Logger.WriteLine($"Generated: {fileName}");
+        }
+
+        private void SavePdf(string name, ReferenceDistribution[] referenceDistributions)
+        {
+            string fileName = $"{name}_Pdf.csv";
+            using var writer = new StreamWriter(fileName);
+            writer.WriteLine("distribution,x,pdf");
+
+            foreach (var referenceDistribution in referenceDistributions)
+            {
+                var distribution = referenceDistribution.Distribution;
+                string distributionName = referenceDistribution.Key;
+
+                double low = distribution.Quantile(0.01);
+                double low2 = distribution.Quantile(0);
+                if (low2 > -1)
+                    low = low2;
+                double high = distribution.Quantile(0.9);
+                double high2 = distribution.Quantile(0.99);
+                if (high2 - low < (high - low) * 3)
+                    high = high2;
+                high2 = distribution.Quantile(1);
+                if (high2 < double.PositiveInfinity && high2 - low < (high - low) * 3)
+                    high = high2;
+                const int n = 1001;
+                double[] xs = Enumerable.Range(0, n).Select(i => low + (high - low) / (n - 1) * i).ToArray();
+                double[] pdfs = xs.Select(x => distribution.Pdf(x)).ToArray();
+                double maxPdf = SimpleQuantileEstimator.Instance.GetQuantile(new Sample(pdfs), 0.99) *
+                                10; // PDF Normalization
+                for (int i = 0; i < xs.Length; i++)
+                {
+                    double x = xs[i], pdf = Math.Min(pdfs[i], maxPdf);
+                    writer.WriteLine($"\"{distributionName}\",{x},{pdf}");
+                }
+            }
+
+            Logger.WriteLine($"Generated: {fileName}");
+        }
+
+        private void SaveDescription(string name, ReferenceDistribution[] referenceDistributions)
+        {
+            string fileName = $"{name}_Description.csv";
+            using var writer = new StreamWriter(fileName);
+            writer.WriteLine("distribution,description");
+            foreach (var distribution in referenceDistributions)
+                writer.WriteLine($"\"{distribution.Key}\",\"{distribution.Description}\"");
+            Logger.WriteLine($"Generated: {fileName}");
+
+            string fileName2 = $"{name}_Description.md";
+            using var writer2 = new StreamWriter(fileName2);
+            foreach (var d in referenceDistributions)
+            {
+                writer2.WriteLine($"* **{d.Key}**: {d.Description}  ");
+                writer2.WriteLine($"  `{d.Distribution}`");
+            }
+
+            Logger.WriteLine($"Generated: {fileName2}");
+        }
+
+        private static void InitCulture()
+        {
+            var culture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+            culture.NumberFormat.NumberDecimalSeparator = ".";
+            culture.NumberFormat.NumberGroupSeparator = "";
+            CultureInfo.CurrentCulture = culture;
+        }
+
+        private static int[] GenerateSequence(int from, int to, int step) =>
+            new ArithmeticProgressionSequence(from, step)
+                .GenerateEnumerable()
+                .TakeWhile(value => value <= to)
+                .Select(value => (int)Math.Round(value))
+                .ToArray();
+    }
+}
