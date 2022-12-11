b82d0439a3b00fd5016a4f7425dfeff8d9be7f0d
Andrey Akinshin <andrey.akinshin@gmail.com>
2022-12-11 12:01:53 +0400

    Introduce color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
 module.exports = {
   darkMode: 'class',
   content: [
@@ -7,7 +9,54 @@ module.exports = {
     "./content/**/*.md"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'back': {
+          'l': colors.white,
+          'd': colors.zinc["800"],
+        },
+        'back-accent': {
+          'l': colors.sky["800"],
+          'd': colors.zinc["900"],
+        },
+        'back-accent-hover': {
+          'l': colors.sky["700"],
+          'd': colors.gray["700"],
+        },
+        'back2': {
+          'l': colors.gray["100"],
+          'd': colors.zinc["700"],
+        },
+        'front': {
+          'l': colors.black,
+          'd': colors.gray["400"],
+        },
+        'accent': {
+          'l': colors.sky["600"],
+          'd': colors.white,
+        },
+        'accent-frame': {
+          'l': colors.sky["600"],
+          'd': colors.slate["400"],
+        },
+        'accent-hover': {
+          'l': colors.sky["200"],
+          'd': colors.gray["500"],
+        },
+        'accent-icon-hover': {
+          'l': colors.sky["400"],
+          'd': colors.blue["400"],
+        },
+        'shadow': {
+          'l': colors.gray["300"],
+          'd': colors.gray["600"],
+        },
+        'frame': {
+          'l': colors.gray["300"],
+          'd': colors.gray["600"],
+        }
+      }
+    }
   },
   plugins: [],
 }
