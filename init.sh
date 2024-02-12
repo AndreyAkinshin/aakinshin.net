96bdb8eed297a3e39750eb559d0238f882d5e948
Andrey Akinshin <andrey.akinshin@gmail.com>
2024-02-12 21:51:32 +0000

    [engine] Bump hugo: 0.102.3-patched2 -> 0.122.0

--- init.sh
+++ init.sh
@@ -8,29 +8,32 @@ else
   TARGET_DIR="./bin"
 fi
 
-HUGO_VER="v0.102.3-patched2"
+HUGO_VER="0.122.0"
 TW_VER="v3.3.5"
 
 ARCH=$(arch)
 
-if [ $ARCH == "x86_64" ]; then
-  ARCH="x64"
+if [ "$ARCH" == "x86_64" ]; then
+  HUGO_ARCH="amd64"
+  TW_ARCH="x64"
 fi
-if [ $ARCH == "aarch64" ]; then
-  ARCH="arm64"
+if [ "$ARCH" == "aarch64" ]; then
+  HUGO_ARCH="arm64"
+  TW_ARCH="arm64"
 fi
 
 if [ "$(uname)" == "Darwin" ]
 then
-  HUGO_URL="https://github.com/AndreyAkinshin/hugo/releases/download/${HUGO_VER}/hugo-macos"
-  TW_URL="https://github.com/tailwindlabs/tailwindcss/releases/download/${TW_VER}/tailwindcss-macos-${ARCH}"
+  HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VER}/hugo_${HUGO_VER}_darwin-universal.tar.gz"
+  TW_URL="https://github.com/tailwindlabs/tailwindcss/releases/download/${TW_VER}/tailwindcss-macos-${TW_ARCH}"
 else
-  HUGO_URL="https://github.com/AndreyAkinshin/hugo/releases/download/${HUGO_VER}/hugo-linux-${ARCH}"
-  TW_URL="https://github.com/tailwindlabs/tailwindcss/releases/download/${TW_VER}/tailwindcss-linux-${ARCH}"
+  HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VER}/hugo_${HUGO_VER}_linux-${HUGO_ARCH}.tar.gz"
+  TW_URL="https://github.com/tailwindlabs/tailwindcss/releases/download/${TW_VER}/tailwindcss-linux-${TW_ARCH}"
 fi
 
-wget $HUGO_URL -O $TARGET_DIR/hugo
-wget $TW_URL -O $TARGET_DIR/tailwind
+wget "$HUGO_URL" -O "$TARGET_DIR/hugo.tar.gz"
+tar -xzvf "$TARGET_DIR/hugo.tar.gz" -C "$TARGET_DIR" hugo
+wget "$TW_URL" -O "$TARGET_DIR/tailwind"
 
-chmod +x $TARGET_DIR/hugo
-chmod +x $TARGET_DIR/tailwind
+chmod +x "$TARGET_DIR/hugo"
+chmod +x "$TARGET_DIR/tailwind"
